89cd7636edaef6a6a6356fe98f190c08d0697796
Damon <petersdamon8@gmail.com>
2024-04-23 14:11:56 -0200

    third

---
+++ about.js
@@ -0,0 +1,13 @@
+const school = document.getElementById("school");
+school.textContent = "homevale high school";
+
+const year = document.getElementById("year")
+year.textContent = "2021";
+const years = document.getElementById("years");
+years.textContent = "2021-2022"
+const school2 = document.getElementById("school2");
+school2.textContent = "homevale high school";
+const Mlad = document.getElementById("mlab");
+Mlad.textContent = "Mlab";
+const years2 = document.getElementById("years2");
+years2.textContent = "2023-2024"
